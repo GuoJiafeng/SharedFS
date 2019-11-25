dda47a48873a47a7ba604822ec74ec3d1601f6bf
GuoJiafeng <18611781163@163.com>
2019-11-25 17:51:51 +0800

    1

--- src/main/java/io/gjf/controller/FileController.java
+++ src/main/java/io/gjf/controller/FileController.java
@@ -2,11 +2,15 @@ package io.gjf.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.servlet.http.HttpServletResponse;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.OutputStream;
+import java.util.UUID;
 
 /**
  * Creat by GuoJF on
@@ -40,4 +44,33 @@ public class FileController {
         }
         fis.close();
     }
+
+
+    @RequestMapping("upload")
+    @ResponseBody
+    public String upload(@RequestParam("file") MultipartFile file) {
+        // 获取原始名字
+        String fileName = file.getOriginalFilename();
+        // 获取后缀名
+        // String suffixName = fileName.substring(fileName.lastIndexOf("."));
+        // 文件保存路径
+        String filePath = BASE_PATH;
+        // 文件重命名，防止重复
+        fileName = filePath + UUID.randomUUID() + fileName;
+        // 文件对象
+        File dest = new File(fileName);
+        // 判断路径是否存在，如果不存在则创建
+        if (!dest.getParentFile().exists()) {
+            dest.getParentFile().mkdirs();
+        }
+        try {
+            // 保存到服务器中
+            file.transferTo(dest);
+            return "上传成功";
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return "上传失败";
+    }
+
 }
